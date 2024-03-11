6cf453732979e60a4ea2f78ea02ed43749468d84
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-03-11 17:42:36 +0530

    Update restart.py

--- SHUKLA/plugins/owner/restart.py
+++ SHUKLA/plugins/owner/restart.py
@@ -10,7 +10,7 @@ from ... import *
 async def restart(client: Client, message: Message):
     reply = await message.reply_text("**Restarting...**")
     await message.delete()
-    await reply.edit_text("Successfully Restarted PbxBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
+    await reply.edit_text("Successfully Restarted ShuklaBot...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
     os.system(f"kill -9 {os.getpid()} && python3 -m PbxTeam")
   
 
