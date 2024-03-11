63dc5b6cb04bbe176eedc64260df7aa164b39f95
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-03-11 16:07:58 +0530

    Add files via upload

---
+++ SHUKLA/__init__.py
@@ -0,0 +1,59 @@
+from SHUKLA.console import  SUDO_USERS, OWNER_ID, OWNER_USERNAME
+from aiohttp import ClientSession
+
+# Ai-Userbot Version
+__version__ = "v2.1.0"
+
+spam_chats = []
+aiosession = ClientSession()
+SUDO_USER = SUDO_USERS
+OWNER_USERNAME = OWNER_USERNAME
+SUDO_USERS.append(OWNER_ID)
+
+
+# All Clients
+from .modules.clients.clients import (
+    app, bot, call
+)
+app = app
+bot = bot
+call = call
+
+
+# Command Handlers
+from .modules.helpers.filters import (
+    commandx, commandz
+)
+cdx = commandx
+cdz = commandz
+
+
+# Edit Or Reply
+from .modules.helpers.events import (
+    edit_or_reply
+)
+eor = edit_or_reply
+
+
+# Logger
+from .console import LOGGER
+logs = LOGGER
+
+
+# Plugins
+from .console import PLUGINS
+plugs = PLUGINS
+
+
+# Variables
+from . import console as config
+vars = config
+
+
+# Decorators
+from .modules.helpers.wrapper import (
+    super_user_only, sudo_users_only
+)
+super_user_only = super_user_only
+sudo_users_only = sudo_users_only
+
--- SHUKLA/console.py
+++ SHUKLA/console.py
@@ -1,1 +1,78 @@
+import os
+import time
+import logging
+
+from os import getenv
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+
+
+logging.basicConfig(
+    format="[%(asctime)s]:[%(levelname)s]:[%(name)s]:: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    handlers=[
+        RotatingFileHandler(
+            "logs.txt", maxBytes=(1024 * 1024 * 5), backupCount=10
+        ),
+        logging.StreamHandler(),
+    ],
+)
+
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+
+
+if os.path.exists("Internal"):
+   load_dotenv("Internal")
+
+
+API_ID = int(getenv("API_ID", 0))
+API_HASH = getenv("API_HASH", None)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING_SESSION = getenv("STRING_SESSION", None)
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+OWNER_ID = int(getenv("OWNER_ID", "6762113050"))
+OWNER_USERNAME = getenv("OWNER_USERNAME", None)
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6919199044").split()))
+ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/7a2e0a67e160deb60e48b.jpg")
+
+
+# OPTIONAL VARIABLES
+SESSION_STRING = getenv("SESSION_STRING", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
+
+
+
+# OTHERS VARIABLES
+
+# PM GUARD VARS
+PM_GUARD = bool(getenv("PM_GUARD", True))
+PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 ʜᴇʏ, ɪ ᴀᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ & ꜱᴜᴘᴇʀꜰᴀꜱᴛ ʜɪɢʜ Qᴜᴀʟɪᴛʏ ᴜꜱᴇʀʙᴏᴛ ᴀꜱꜱɪꜱᴛᴀɴᴛ ᴡɪᴛʜ ᴀɴ ᴜᴘɢʀᴀᴅᴇᴅ ᴠᴇʀꜱɪᴏɴ ꜱᴇᴄᴜʀɪᴛʏ ꜱʏꜱᴛᴇᴍ.\n\n🌿 ɪ ᴄᴀɴ'ᴛ ʟᴇᴛ ʏᴏᴜ ᴍᴇꜱꜱᴀɢᴇ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴅᴍ ᴡɪᴛʜᴏᴜᴛ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴘᴇʀᴍɪꜱꜱɪᴏɴ.\n\n❤️ ᴍʏ ᴏᴡɴᴇʀ ɪꜱ ᴏꜰꜰʟɪɴᴇ ɴᴏᴡ, ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴜɴᴛɪʟ ᴍʏ ᴏᴡɴᴇʀ ᴀʟʟᴏᴡꜱ ʏᴏᴜ.\n\n🍂 ᴘʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ꜱᴘᴀᴍ ʜᴇʀᴇ, ʙᴇᴄᴀᴜꜱᴇ ꜱᴘᴀᴍᴍɪɴɢ ᴡɪʟʟ ꜰᴏʀᴄᴇ ᴍᴇ ᴛᴏ ʙʟᴏᴄᴋ ʏᴏᴜ ꜰʀᴏᴍ ᴍʏ ᴏᴡɴᴇʀ ɪᴅ 👍🏻**")
+PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
+
+
+# USERBOT DEFAULT IMAGE
+USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://telegra.ph/file/7a2e0a67e160deb60e48b.jpg")
+
+
+
+# Don't Edit This Codes From This Line
+
+LOGGER = logging.getLogger("SHASHANK")
+runtime = time.time()
+
+FLOODXD = {}
+OLD_MSG = {}
+PM_LIMIT = {}
+PLUGINS = {}
+SUDOERS = []
+
+
+COMMAND_HANDLERS = []
+for x in COMMAND_PREFIXES:
+    COMMAND_HANDLERS.append(x)
+COMMAND_HANDLERS.append('')
 
