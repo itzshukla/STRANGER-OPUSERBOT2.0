e33e238bbc29ec74fb649f14a326882274b6875d
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-03-11 16:51:23 +0530

    Add files via upload

---
+++ SHUKLA/plugins/raids/raidzone.py
@@ -0,0 +1,17 @@
+# Powered By // @SHIVANSH474 //
+
+__NAME__ = "Rᴀɪᴅs"
+__MENU__ = """
+**Reply On Target Users Messages
+To Activate Or Deactivate.**
+
+`.cr` - Activate Chat Raid.
+`.dcr` - De-Activate Chat Raid.
+
+`.fr` - Activate Fuck Raid.
+`.dfr` - De-Activate Fuck Raid.
+
+`.lr` - Activate Love Raid.
+`.dlr` - De-Activate Love Raid.
+"""
+
