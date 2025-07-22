226a09c0004e822b682ac66cd30b2ed8e42dfa8a
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-23 01:12:09 +0530

    Update streams.py

--- SHUKLA/modules/utilities/streams.py
+++ SHUKLA/modules/utilities/streams.py
@@ -1,19 +1,13 @@
-import os
-import asyncio
-import yt_dlp
+import asyncio, os, yt_dlp
 
 from . import queues
 from ..clients.clients import call
 from ...console import USERBOT_PICTURE
 
 from asyncio.queues import QueueEmpty
-from pytgcalls.types.stream import Stream
-from pytgcalls.types.input_stream import (
-    AudioStream,
-    AudioParameters,
-    VideoStream,
-    VideoParameters,
-)
+from ntgcalls import InputMode
+from pytgcalls.types import *
+from pytgcalls.types.input_stream import *
 from youtubesearchpython.__future__ import VideosSearch
 
 
@@ -26,49 +20,69 @@ async def get_result(query: str):
     results = VideosSearch(query, limit=1)
     for result in (await results.next())["result"]:
         url = result["link"]
-        thumbnail = result.get("thumbnails", [{}])[0].get("url", USERBOT_PICTURE).split("?")[0]
-        return url, thumbnail
-    return None, USERBOT_PICTURE
+        try:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        except:
+            thumbnail = USERBOT_PICTURE
+
+    return url, thumbnail
 
 
-async def get_stream(link: str, type_: str) -> str:
-    ydl_opts = {
-        "outtmpl": "downloads/%(id)s.%(ext)s",
-        "geo_bypass": True,
-        "nocheckcertificate": True,
-        "quiet": True,
-        "no_warnings": True,
-    }
+async def get_stream(link, type):
+    if type == "Audio":
+        ydl_opts = {
+            "format": "bestaudio/best",
+            "outtmpl": "downloads/%(id)s.%(ext)s",
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "quiet": True,
+            "no_warnings": True,
+        }
 
-    if type_ == "Audio":
-        ydl_opts["format"] = "bestaudio/best"
-    elif type_ == "Video":
-        ydl_opts["format"] = "(bestvideo[height<=720][width<=1280][ext=mp4])+(bestaudio[ext=m4a])"
+    elif type == "Video":
+        ydl_opts = {
+            "format": "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])",
+            "outtmpl": "downloads/%(id)s.%(ext)s",
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "quiet": True,
+            "no_warnings": True,
+        }
 
     x = yt_dlp.YoutubeDL(ydl_opts)
-    info = x.extract_info(link, download=False)
-    file_path = os.path.join("downloads", f"{info['id']}.{info['ext']}")
-
-    if not os.path.exists(file_path):
-        await run_async(x.download, [link])
-
-    return file_path
-
-
-async def run_stream(file: str, type_: str) -> Stream:
-    audio_stream = AudioStream(
-        path=f"ffmpeg -i {file} -f s16le -ac 2 -ar 48000 pipe:1",
-        parameters=AudioParameters(
-            bitrate=48000,
-            channels=2,
-        ),
+    info = x.extract_info(link, False)
+    file = os.path.join(
+        "downloads", f"{info['id']}.{info['ext']}"
     )
+    if os.path.exists(file):
+        return file
+    await run_async(x.download, [link])
+    return file
 
-    if type_ == "Audio":
-        return Stream(audio_stream)
 
-    elif type_ == "Video":
+async def run_stream(file, type):
+    if type == "Audio":
+        audio_stream = AudioStream(
+            input_mode=InputMode.Shell,
+            path=f"ffmpeg -i {file} -f s16le -ac 2 -ar 48k pipe:1",
+            parameters=AudioParameters(
+                bitrate=48000,
+                channels=2,
+            ),
+        )
+        stream = Stream(audio_stream)
+
+    elif type == "Video":
+        audio_stream = AudioStream(
+            input_mode=InputMode.Shell,
+            path=f"ffmpeg -i {file} -f s16le -ac 2 -ar 48k pipe:1",
+            parameters=AudioParameters(
+                bitrate=48000,
+                channels=2,
+            ),
+        )
         video_stream = VideoStream(
+            input_mode=InputMode.Shell,
             path=f"ffmpeg -i {file} -f rawvideo -r 30 -pix_fmt yuv420p -vf scale=1280:720 pipe:1",
             parameters=VideoParameters(
                 width=1280,
@@ -76,15 +90,18 @@ async def run_stream(file: str, type_: str) -> Stream:
                 frame_rate=30,
             ),
         )
-        return Stream(audio_stream, video_stream)
+        stream = Stream(audio_stream, video_stream)
+
+    return stream
 
 
-async def close_stream(chat_id: int):
+
+async def close_stream(chat_id):
     try:
-        queues.clear(chat_id)
+        await queues.clear(chat_id)
     except QueueEmpty:
         pass
     try:
-        await call.leave_group_call(chat_id)
-    except Exception:
+        return await call.leave_group_call(chat_id)
+    except:
         pass
