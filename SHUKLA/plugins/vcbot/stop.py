d9ceb37e5f2456be4b48f31d326903d90cf1acaf
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-23 01:48:59 +0530

    Update stop.py

--- SHUKLA/plugins/vcbot/stop.py
+++ SHUKLA/plugins/vcbot/stop.py
@@ -1,6 +1,5 @@
 from asyncio.queues import QueueEmpty
 from pyrogram import filters
-from pytgcalls.exceptions import GroupCallNotFound
 
 from ... import app, eor, cdx, cdz, call
 from ...modules.helpers.wrapper import sudo_users_only
