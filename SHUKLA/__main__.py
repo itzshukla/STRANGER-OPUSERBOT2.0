f1ba25a4c3675239c45a83643907a54240901366
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-22 23:31:27 +0530

    Update __main__.py

--- SHUKLA/__main__.py
+++ SHUKLA/__main__.py
@@ -10,34 +10,32 @@ from .modules.helpers.inline import run_async_inline
 loop = asyncio.get_event_loop()
 
 async def main():
-    # Call the start method of the Shukla instance
-    await run_async_clients.start()
+    await run_async_clients()
+
     for all_plugin in ALL_PLUGINS:
         imported_plugin = importlib.import_module(
             "SHUKLA.plugins" + all_plugin
         )
-        if (hasattr
-            (
-                imported_plugin, "__NAME__"
-            ) and imported_plugin.__NAME__
+        if (
+            hasattr(imported_plugin, "__NAME__")
+            and imported_plugin.__NAME__
         ):
             imported_plugin.__NAME__ = imported_plugin.__NAME__
             if (
-                hasattr(
-                    imported_plugin, "__MENU__"
-                ) and imported_plugin.__MENU__
+                hasattr(imported_plugin, "__MENU__")
+                and imported_plugin.__MENU__
             ):
-                plugs[imported_plugin.__NAME__.lower()
-                ] = imported_plugin
+                plugs[imported_plugin.__NAME__.lower()] = imported_plugin
+
     await run_async_enums()
     logs.info(">> Successfully Imported All Plugins.")
     await run_async_inline()
     logs.info("Successfully Deployed !!")
     logs.info("Do Visit - @MASTIWITHFRIENDSXD")
+
     await idle()
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     print("Userbot Stopped !\nGoodBye ...")
