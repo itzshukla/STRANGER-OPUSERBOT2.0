ea49bba2cc302f46d37837d59e90bf9855084235
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-22 23:59:43 +0530

    Update joinvc.py

--- SHUKLA/plugins/vcbot/joinvc.py
+++ SHUKLA/plugins/vcbot/joinvc.py
@@ -1,45 +1,39 @@
-from ... import *
-from ...modules.mongo.streams import *
 from pyrogram import filters
-from pytgcalls.exceptions import GroupCallNotFound
+from ... import app, eor, cdx, cdz, call
+from ...modules.mongo.streams import get_chat_id
+from ...modules.helpers.wrapper import sudo_users_only
 
 
 @app.on_message(cdx(["join", "joinvc"]) & ~filters.private)
 @sudo_users_only
 async def join_vc(client, message):
     chat_id = message.chat.id
+    if call.is_running(chat_id):
+        return await eor(message, "**✅ Already joined VC!**")
+
     try:
-        a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
-            or a.status == "playing"
-            or a.status == "paused"
-        ):
-            await eor(message, "**Already Joined!**")
-    except GroupCallNotFound:
-        await call.join_group_call(chat_id)
-        await eor(message, "**Joined VC!**")
+        await call.join_call(chat_id)
+        await eor(message, "**🎧 Joined VC successfully!**")
     except Exception as e:
         print(f"Error: {e}")
+        await eor(message, "**❌ Failed to join VC!**")
 
 
 @app.on_message(cdz(["cjoin", "cjoinvc"]))
 @sudo_users_only
-async def join_vc_(client, message):
+async def join_vc_chat(client, message):
     user_id = message.from_user.id
     chat_id = await get_chat_id(user_id)
+
     if chat_id == 0:
-        return await eor(message,
-            "**🥀 No Stream Chat Set❗**"
-    )
+        return await eor(message, "**🥀 No Stream Chat Set❗**")
+
+    if call.is_running(chat_id):
+        return await eor(message, "**✅ Already joined VC!**")
+
     try:
-        a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
-            or a.status == "playing"
-            or a.status == "paused"
-        ):
-            await eor(message, "**Already Joined!**")
-    except GroupCallNotFound:
-        await call.join_group_call(chat_id)
-        await eor(message, "**Joined VC!**")
+        await call.join_call(chat_id)
+        await eor(message, "**🎧 Joined VC successfully!**")
     except Exception as e:
         print(f"Error: {e}")
+        await eor(message, "**❌ Failed to join VC!**")
