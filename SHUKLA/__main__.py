2ac309b82d069af029b1df7f704eca080937bedd
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-22 23:35:56 +0530

    Update __main__.py

--- SHUKLA/__main__.py
+++ SHUKLA/__main__.py
@@ -7,29 +7,21 @@ from .modules.clients.clients import run_async_clients
 from .modules.clients.enums import run_async_enums
 from .modules.helpers.inline import run_async_inline
 
-loop = asyncio.get_event_loop()
 
 async def main():
     await run_async_clients()
 
     for all_plugin in ALL_PLUGINS:
-        imported_plugin = importlib.import_module(
-            "SHUKLA.plugins" + all_plugin
-        )
-        if (
-            hasattr(imported_plugin, "__NAME__")
-            and imported_plugin.__NAME__
-        ):
+        imported_plugin = importlib.import_module(f"SHUKLA.plugins{all_plugin}")
+        if hasattr(imported_plugin, "__NAME__") and imported_plugin.__NAME__:
             imported_plugin.__NAME__ = imported_plugin.__NAME__
-            if (
-                hasattr(imported_plugin, "__MENU__")
-                and imported_plugin.__MENU__
-            ):
+            if hasattr(imported_plugin, "__MENU__") and imported_plugin.__MENU__:
                 plugs[imported_plugin.__NAME__.lower()] = imported_plugin
 
     await run_async_enums()
     logs.info(">> Successfully Imported All Plugins.")
     await run_async_inline()
+
     logs.info("Successfully Deployed !!")
     logs.info("Do Visit - @MASTIWITHFRIENDSXD")
 
@@ -37,5 +29,6 @@ async def main():
 
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-    print("Userbot Stopped !\nGoodBye ...")
+    print("Userbot Stopped!\nGoodbye ...")
