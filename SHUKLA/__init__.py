c35ab1048d6fd88ec1c4ba73402cde73f206071f
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-23 14:08:52 +0530

    Update __init__.py

--- SHUKLA/__init__.py
+++ SHUKLA/__init__.py
@@ -1,59 +1,70 @@
-from SHUKLA.console import  SUDO_USERS, OWNER_ID, OWNER_USERNAME
-from aiohttp import ClientSession
+import os
 
-# Ai-Userbot Version
-__version__ = "v2.1.0"
+from .console import LOGGER
+from .modules.clients.clients import Shukla
+from .modules.clients.vars import Config
+from .modules.clients.command import *
 
+__version__ = "v2.0.1"
+
+if Config.API_ID == 0:
+    LOGGER.error("API_ID is missing! Kindly check again!")
+    exit()
+if not Config.API_HASH:
+    LOGGER.error("API_HASH is missing! Kindly check again!")
+    exit()
+if not Config.BOT_TOKEN:
+    LOGGER.error("BOT_TOKEN is missing! Kindly check again!")
+    exit()
+if not Config.STRING_SESSION:
+    LOGGER.error("STRING_SESSION is missing! Kindly check again!")
+    exit()
+if not Config.MONGO_DATABASE:
+    LOGGER.error("DATABASE_URL is missing! Kindly check again!")
+    exit()
+if Config.LOG_GROUP_ID == 0:
+    LOGGER.error("LOG_GROUP_ID is missing! Kindly check again!")
+    exit()
+
+for file in os.listdir():
+    if file.endswith(".session"):
+        os.remove(file)
+for file in os.listdir():
+    if file.endswith(".session-journal"):
+        os.remove(file)
+
+
+shukla = Shukla()
+app = shukla.app
+bot = shukla.bot
+call = shukla.call
+log = LOGGER
+var = Config()
+
+db = {}
+flood = {}
+OLD_MSG = {}
 spam_chats = []
-aiosession = ClientSession()
-SUDO_USER = SUDO_USERS
-OWNER_USERNAME = OWNER_USERNAME
-SUDO_USERS.append(OWNER_ID)
 
-
-# All Clients
-from .modules.clients.clients import (
-    app, bot, call
-)
-app = app
-bot = bot
-call = call
-
-
-# Command Handlers
-from .modules.helpers.filters import (
-    commandx, commandz
-)
 cdx = commandx
 cdz = commandz
 
 
-# Edit Or Reply
-from .modules.helpers.events import (
-    edit_or_reply
-)
-eor = edit_or_reply
+PLUGINS = var.PLUGINS
+SUPUSER = var.SUPUSER
+SUDOERS = var.SUDOERS
+OWNER_ID = Config.OWNER_ID
+SUDO_USER = Config.SUDO_USERS
+OWNER_USERNAME = Config.OWNER_USERNAME
+Config.SUDO_USERS.append(Config.OWNER_ID)
 
 
-# Logger
-from .console import LOGGER
-logs = LOGGER
+from .modules.clients.command import eor
+eor = eor
 
-
-# Plugins
-from .console import PLUGINS
-plugs = PLUGINS
-
-
-# Variables
-from . import console as config
-vars = config
-
-
-# Decorators
 from .modules.helpers.wrapper import (
     super_user_only, sudo_users_only
 )
-super_user_only = super_user_only
-sudo_users_only = sudo_users_only
 
+from .modules.clients.vars import Config
+vars = Config
