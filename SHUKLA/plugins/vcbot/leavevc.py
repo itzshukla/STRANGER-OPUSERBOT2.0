cde4279907811521fcb97969013802d5b48e5310
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-07-23 13:47:20 +0530

    Update leavevc.py

--- SHUKLA/plugins/vcbot/leavevc.py
+++ SHUKLA/plugins/vcbot/leavevc.py
@@ -1,39 +1,56 @@
 from asyncio.queues import QueueEmpty
 from pyrogram import filters
-from ... import app, eor, cdx, cdz, call
-from ...modules.mongo.streams import get_chat_id
+from pytgcalls.exceptions import GroupCallNotFound
+
+from ... import *
+from ...modules.mongo.streams import *
 from ...modules.utilities import queues
-from ...modules.helpers.wrapper import sudo_users_only
 
 
 @app.on_message(cdx(["lve", "leave", "leavevc"]) & ~filters.private)
 @sudo_users_only
 async def leave_vc(client, message):
     chat_id = message.chat.id
-    if not call.is_running(chat_id):
-        return await eor(message, "**❌ I am not in VC!**")
     try:
-        queues.clear(chat_id)
-    except QueueEmpty:
-        pass
-    await call.leave_call(chat_id)
-    await eor(message, "**✅ Left VC successfully!**")
+        a = await call.get_call(chat_id)
+        if (a.status == "not_playing"
+            or a.status == "playing"
+            or a.status == "paused"
+        ):  
+            try:
+                queues.clear(chat_id)
+            except QueueEmpty:
+                pass
+            await call.leave_group_call(chat_id)
+            await eor(message, "**Left VC!**")
+    except GroupCallNotFound:
+        await eor(message, "**I am Not in VC!**")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 @app.on_message(cdz(["clve", "cleave", "cleavevc"]))
 @sudo_users_only
-async def leave_vc_chat(client, message):
+async def leave_vc_(client, message):
     user_id = message.from_user.id
     chat_id = await get_chat_id(user_id)
     if chat_id == 0:
-        return await eor(message, "**🥀 No Stream Chat Set❗**")
-
-    if not call.is_running(chat_id):
-        return await eor(message, "**❌ I am not in VC!**")
-
+        return await eor(message,
+            "**🥀 No Stream Chat Set❗**"
+    )
     try:
-        queues.clear(chat_id)
-    except QueueEmpty:
-        pass
-    await call.leave_call(chat_id)
-    await eor(message, "**✅ Left VC successfully!**")
+        a = await call.get_call(chat_id)
+        if (a.status == "not_playing"
+            or a.status == "playing"
+            or a.status == "paused"
+        ):  
+            try:
+                queues.clear(chat_id)
+            except QueueEmpty:
+                pass
+            await call.leave_group_call(chat_id)
+            await eor(message, "**Left VC!**")
+    except GroupCallNotFound:
+        await eor(message, "**I am Not in VC!**")
+    except Exception as e:
+        print(f"Error: {e}")
