d04a024fe50d40eebe11ff81adf109f9fc7bf864
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-08-11 19:19:08 +0530

    Update ai.py

--- SHUKLA/plugins/tools/ai.py
+++ SHUKLA/plugins/tools/ai.py
@@ -30,7 +30,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @SHIVANSH474l",
+                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @ITSZSHUKLA",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
